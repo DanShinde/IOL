006ea3526311ae83faa447833bfff2094dfcb2c8
DanShinde <pravin113347@gmail.com>
2023-03-22 16:29:06 +0530

    Updated IO Address Generation

--- core/iol/views.py
+++ core/iol/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from io import BytesIO
 import json
+import math
 import subprocess
 from django.forms import inlineformset_factory
 from django.http import HttpRequest, HttpResponse, JsonResponse
@@ -136,30 +137,112 @@ def add_signals(request):
 
     return JsonResponse({'success': True, 'data': data})
 
+def add_spares(worksheet,row, project, IO, count,I_Pointer, Q_Pointer):
+    worksheet.write(row, 0, row+1)
+    worksheet.write(row, 1, "Spare")
+    worksheet.write(row, 2, "Spare")
+    if IO.signal_type == "DI":
+        worksheet.write(row, 3, f'Ix_Spare_Spare_{count}')
+    elif IO.signal_type == "DO":
+        worksheet.write(row, 3, f'Qx_Spare_Spare_{count}')
+    worksheet.write(row, 4, IO.signal_type)
+    worksheet.write(row, 5, "Spare_Signal")
+    if IO.signal_type == "DI":
+        print(f'I_Pointer {I_Pointer}.')
+        x = f"I{str(math.floor((I_Pointer - 1) / 8))}.{str((I_Pointer - 1) % 8)}"
+        print(x)
+    elif IO.signal_type == "DO":
+        x = f"Q{str(math.floor((Q_Pointer - 1) / 8))}.{str((Q_Pointer - 1) % 8)}"
+        print(x)
+    worksheet.write(row, 6, x)
+    worksheet.write(row, 7, "Spare Signal")
+    worksheet.write(row, 8, IO.panel_number)
+    worksheet.write(row, 9, "CP")
+    return worksheet, I_Pointer, Q_Pointer
 
 #function to write indivisual sheets while exporting
-def write_sheet(panel,workbook, project, iolist):
+def write_sheet(panel,workbook, project, iolist, I_Pointer, Q_Pointer):
     worksheet = workbook.add_worksheet(panel)
     bold = workbook.add_format({'bold': True})
-    columns = ["Project", "ModuleName",  "Code", "Tag", "Signal Type","Device Type", "Actual Description","Panel Number","Location"]
+    columns = ["Project", "ModuleName",  "Code", "Tag", "Signal Type","Device Type","I/O Address","Actual Description","Panel Number","Location"]
     # Fill first row with columns
     row = 0
     for i,elem in enumerate(columns):
         worksheet.write(row, i, elem, bold)
     row += 1
     # Now fill other rows with columns
+    i = 0
+    q = 0
+    # Writing DIs to file
     for IO in iolist:
-        worksheet.write(row, 0, project.name)
-        worksheet.write(row, 1, IO.name)
-        worksheet.write(row, 2, IO.code)
-        worksheet.write(row, 3, IO.tag)
-        worksheet.write(row, 4, IO.signal_type)
-        worksheet.write(row, 5, IO.device_type)
-        worksheet.write(row, 6, IO.actual_description)
-        worksheet.write(row, 7, IO.panel_number)
-        worksheet.write(row, 8, IO.location)
+        if IO.signal_type == "DI":
+            worksheet.write(row, 0, project.name)
+            worksheet.write(row, 1, IO.name)
+            worksheet.write(row, 2, IO.code)
+            worksheet.write(row, 3, IO.tag)
+            worksheet.write(row, 4, IO.signal_type)
+            worksheet.write(row, 5, IO.device_type)
+            if IO.signal_type == "DI":
+                x = f"I{str(math.floor((I_Pointer - 1) / 8))}.{str((I_Pointer - 1) % 8)}"
+                I_Pointer+= 1
+            elif IO.signal_type == "DO":
+                x = f"Q{str(math.floor((Q_Pointer - 1) / 8))}.{str((Q_Pointer - 1) % 8)}"
+                Q_Pointer+= 1
+            IO.io_address =  x
+            IO.save()
+            worksheet.write(row, 6, IO.io_address)
+            worksheet.write(row, 7, IO.actual_description)
+            worksheet.write(row, 8, IO.panel_number)
+            worksheet.write(row, 9, IO.location)
+            row += 1
+            i+= 1
+            q+= 1
+            IOOut = IO
+    print(IOOut.signal_type)
+    print(F'Panel number {panel} of I_Pointer count {I_Pointer}.')
+    #Adding Input spares
+    for i in range(0,16- ( ((I_Pointer-1)%16))):
+        count = i+1
+        worksheet, I_Pointer, Q_Pointer = add_spares(worksheet,row,project,IOOut, count, I_Pointer, Q_Pointer)
+        I_Pointer+= 1
         row += 1
-    return workbook
+        print(I_Pointer)
+
+    #Writing DOs to file
+    for IO in iolist:
+        if IO.signal_type == "DO":
+            worksheet.write(row, 0, project.name)
+            worksheet.write(row, 1, IO.name)
+            worksheet.write(row, 2, IO.code)
+            worksheet.write(row, 3, IO.tag)
+            worksheet.write(row, 4, IO.signal_type)
+            worksheet.write(row, 5, IO.device_type)
+            if IO.signal_type == "DI":
+                x = f"I{str(math.floor((I_Pointer - 1) / 8))}.{str((I_Pointer - 1) % 8)}"
+                I_Pointer+= 1
+            elif IO.signal_type == "DO":
+                x = f"Q{str(math.floor((Q_Pointer - 1) / 8))}.{str((Q_Pointer - 1) % 8)}"
+                Q_Pointer+= 1
+            IO.io_address =  x
+            IO.save()
+            worksheet.write(row, 6, IO.io_address)
+            worksheet.write(row, 7, IO.actual_description)
+            worksheet.write(row, 8, IO.panel_number)
+            worksheet.write(row, 9, IO.location)
+            row += 1
+            i+= 1
+            q+= 1
+            IOOut = IO
+    print(F'Panel number {panel} of I_Pointer count {Q_Pointer}.')
+    #Adding Output spares
+    for i in range(0,16- ( ((Q_Pointer-1)%16))):
+        count = i+1
+        worksheet, I_Pointer, Q_Pointer = add_spares(worksheet,row,project,IOOut, count, I_Pointer, Q_Pointer)
+        row += 1
+        Q_Pointer+= 1
+        print(Q_Pointer)
+    print(F'Panel number {panel} of count {I_Pointer}, {Q_Pointer}.')
+    return workbook, I_Pointer, Q_Pointer
 
 
 #EXporting IO List to Excel file
@@ -168,19 +251,23 @@ def export_to_excel(request):
     project_id = request.session.get('project')
     project = get_object_or_404(Project, id=project_id)
     iolist = IOList.objects.filter(project_id=project_id).order_by('signal_type', 'location')
-    panels = IOList.objects.filter(
-    id=Subquery(
-        IOList.objects.filter(panel_number=OuterRef('panel_number')).order_by('id').values('id')[:1]
-        )
-    )
-    for panel in panels:
-        print(panel.panel_number)
+    panels = [i.panel_number for i in iolist]
+    panels =[*set(panels)]
+    print(panels)
+    I_Pointer = 1
+    Q_Pointer = 1
     output = BytesIO()
     # Feed a buffer to workbook
     workbook = xlsxwriter.Workbook(output)
-    for panel in panels:
-        panelIO = iolist.filter(panel_number=panel.panel_number)
-        workbook = write_sheet(panel.panel_number, workbook, project, panelIO)
+    
+    while None in panels:
+        # removing None from list using remove method
+        panels.remove(None)
+    
+    for panel in sorted(panels):
+        print(panel)
+        panelIO = iolist.filter(panel_number=panel)
+        workbook, I_Pointer, Q_Pointer = write_sheet(panel, workbook, project, panelIO, I_Pointer, Q_Pointer )
 
     workbook.close()
     output.seek(0)
@@ -231,7 +318,8 @@ def edit_module(request, module_id):
     return redirect('/signals')
  
 def iolist_project(request, project_id):
-    return redirect(f'/signals/{project_id}/get')
+    request.session['project'] = project_id
+    return redirect(f'/iolist/{project_id}/get')
 
 class IolistView(View):
 
@@ -250,13 +338,12 @@ class IolistView(View):
     def delete(self, request, *args, **kwargs):
         # sourcery skip: class-extract-method
         iolist = get_object_or_404(IOList, id=kwargs.get('pk'))
+        project= iolist.project
         iolist.delete()
         redirect_url = reverse('iolist')
         
         response = {'valid': 'success', 'message': 'Item deleted successfully', 'redirect_url': redirect_url}
-        
-        # return JsonResponse(response)
-        return redirect('iolist')
+        return redirect((f'/iolist/{project.id}/get'))
     
 
     def edit(self, request, *args, **kwargs):
--- core/sorting/utils.py
+++ core/sorting/utils.py
@@ -1,6 +1,10 @@
 from iol.models import IOList
 from django.db.models import Max
 
+# def get_max_order(project) -> int:
+#     IOs = IOList.objects.filter(project_id = project).aggregate(Max('order'))
+#     return IOs['order__max'] + 1 if len(IOs)>0 else 1
+
 def get_max_order(project) -> int:
     IOs = IOList.objects.filter(project_id = project).aggregate(Max('order'))
-    return IOs['order__max'] + 1 if len(IOs)>0 else 1
+    return max(IOs['order__max'] + 1, 1) if IOs['order__max'] is not None else 1
