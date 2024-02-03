963bdb04cfd1e85e02ba09dc80a5d00391be9629
DanShinde <112338485+DanShinde@users.noreply.github.com>
2024-02-03 17:53:22 +0530

    added dashboard

--- core/dashboard/views.py
+++ core/dashboard/views.py
@@ -5,11 +5,12 @@ import pandas as pd
 # Create your views here.
 def Trends():  # sourcery skip: inline-immediately-returned-variable
     queryset= ProjectReport.objects.all()
-    data = pd.DataFrame(queryset)
-    print(data)
-    # pivot  = data.pivot(columns='segment', values= ['segment'])
-    pivot = pd.pivot_table(data, values = 'project', index='created_by', columns = 'segment').reset_index()
-    return pivot
+    data = pd.DataFrame(queryset.values('project', 'updated_at', 'updated_by', 'created_at', 'created_by', 'segment'))
+    # print(data)
+    # pivot  = data.pivot(columns='segment', values= ['segment'])index='created_by',
+    pivot = pd.pivot_table(data, values='project',  columns='segment', aggfunc='count', fill_value=0)
+    print(pivot.to_json)
+    return pivot.to_json
 
 
 def home(request):
