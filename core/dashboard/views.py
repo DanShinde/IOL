9aa8861a7608ae0153b8a3b12021ae34670a01d5
DanShinde <112338485+DanShinde@users.noreply.github.com>
2024-02-04 15:20:08 +0530

    Updated charts

--- core/dashboard/views.py
+++ core/dashboard/views.py
@@ -1,19 +1,35 @@
 from django.shortcuts import render
 from iol.models import ProjectReport
 import pandas as pd
+from datetime import datetime
 
-# Create your views here.
-def Trends():  # sourcery skip: inline-immediately-returned-variable
-    queryset= ProjectReport.objects.all()
+
+def format_month_year(period):
+    label_str = str(period)  # Convert Period object to string
+    year, month = label_str.split('-')
+    month_name = datetime.strptime(f'{year}-{month}', '%Y-%m').strftime('%B %Y')
+    return month_name
+
+def Trends():
+    queryset = ProjectReport.objects.all()
     data = pd.DataFrame(queryset.values('project', 'updated_at', 'updated_by', 'created_at', 'created_by', 'segment'))
-    # print(data)
-    # pivot  = data.pivot(columns='segment', values= ['segment'])index='created_by',
-    pivot = pd.pivot_table(data, values='project',  columns='segment', aggfunc='count', fill_value=0)
-    data['created_at'] = pd.to_datetime(data['created_at']).dt.to_period('M')
-    pivot2 = pd.pivot_table(data, values='project',  columns='created_at', aggfunc='count', fill_value=0)
-    print(pivot2.to_json)
 
-    return pivot.to_json, pivot2.to_json
+    # Pivot by segment
+    pivot = pd.pivot_table(data, values='project', columns='segment', aggfunc='count', fill_value=0)
+
+    # Format 'created_at' to month and year
+    data['created_at'] = pd.to_datetime(data['created_at']).dt.to_period('M')
+
+    # Pivot by month and year
+    pivot2 = pd.pivot_table(data, values='project', columns='created_at', aggfunc='count', fill_value=0)
+
+    # Format column labels in pivot2
+    pivot2.columns = map(format_month_year, pivot2.columns)
+
+    return pivot.to_json(), pivot2.to_json()
+
+
+
 
 
 def home(request):
