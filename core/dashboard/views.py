a7aa336ffe1d0ff11e3dfef94504e7c8fe0420f9
DanShinde <112338485+DanShinde@users.noreply.github.com>
2024-02-04 15:10:20 +0530

    updated dashboard

--- core/dashboard/views.py
+++ core/dashboard/views.py
@@ -9,13 +9,18 @@ def Trends():  # sourcery skip: inline-immediately-returned-variable
     # print(data)
     # pivot  = data.pivot(columns='segment', values= ['segment'])index='created_by',
     pivot = pd.pivot_table(data, values='project',  columns='segment', aggfunc='count', fill_value=0)
-    print(pivot.to_json)
-    return pivot.to_json
+    data['created_at'] = pd.to_datetime(data['created_at']).dt.to_period('M')
+    pivot2 = pd.pivot_table(data, values='project',  columns='created_at', aggfunc='count', fill_value=0)
+    print(pivot2.to_json)
+
+    return pivot.to_json, pivot2.to_json
 
 
 def home(request):
-    pivot = Trends()
+    pivot, pivot2 = Trends()
     context = {
         'pivot': pivot,
+        'pivot2': pivot2,
+
     }
     return render(request, 'dashboard/dash.html', context)
